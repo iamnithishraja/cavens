81d160e009239aad7a62d44b9fe0e113eb347640
Madni <mohammedmadni09@gmail.com>
2025-08-19 17:01:16 +0530

    feat: add userRoute in main file

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -2,24 +2,29 @@ import express from "express";
 import dotenv from "dotenv";
 import { connectDatabse } from "./config/database";
 import cors from "cors";
+import userRoute from "./routes/userRoute";
 
 dotenv.config();
 const app = express();
 
 connectDatabse();
 
-app.use(cors({
+app.use(
+  cors({
     origin: "*",
-    credentials: true
-  }));
+    credentials: true,
+  })
+);
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.get("/", (req, res) => {
-    res.send("Hello World");
+  res.send("Hello World");
 });
 
+app.use("/api/user", userRoute);
+
 app.listen(process.env.PORT, () => {
-    console.log(`Server is running on port ${process.env.PORT}`);
+  console.log(`Server is running on port ${process.env.PORT}`);
 });
