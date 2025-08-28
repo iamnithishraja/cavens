4e29d8d8c8c431a55b407d95c86911b86ed5a6f1
Nithish Raja <nithishraja@Nithishs-MacBook-Air-2.local>
2025-08-28 20:13:06 +0530

    logout form admin dev

--- frontend/app/(tabs)/adminTabs/index.tsx
+++ frontend/app/(tabs)/adminTabs/index.tsx
@@ -1,11 +1,18 @@
+import { store } from '@/utils';
+import { router } from 'expo-router';
 import React from 'react';
-import { Text } from 'react-native';
+import { Button, Text } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
 
 export default function AnalyticsScreen() {
   return (
     <SafeAreaView>
       <Text>Analytics Screen</Text>
+      <Button title="Logout" onPress={() => {
+        store.delete('user');
+        store.delete('token');
+        router.replace('/auth/Auth');
+      }} />
     </SafeAreaView>
   );
 }
